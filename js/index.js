63e6f4e6d9f440be091212334066d418af7fc1cb
Diana <sundgirl19@gmail.com>
2018-10-10 13:13:50 +0000

    Project

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelector('.modal-contact-form').addEventListener('click', viewModalFormContact);
+function viewModalFormContact(){
+    document.querySelector('.modal-contact-form').style.display="block";
+}
